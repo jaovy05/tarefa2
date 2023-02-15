967112b4e3835a7470823e1bc2bdcef0f61b75d7
Eduardo <evitoryt1@gmail>
2023-02-15 20:28:15 -0300

    alteração

---
+++ arquivotarefa.py
@@ -0,0 +1,82 @@
+def resumo():
+    mensagem = ""
+    return mensagem
+
+
+def doutorado():
+    mensagem = "Em 1926, Von Neumann conseguiu o doutorado em matemática pela universidade de Budapeste."
+    return mensagem
+
+
+def contribuicoes():
+    mensagem = ""
+    return mensagem
+
+
+def artigos():
+    mensagem = "Alguns artigos publicados foram: Theory of self-reproducing automata, On the introduction of transfinite numbers,  A spectral theory for general operators of a unitary space\nmpact of Atomic Energy on the Physical and Chemical Sciences"
+    return mensagem
+
+
+def citacoes():
+    mensagem = "Em matemática não percebemos coisas. Apenas nos habituamos a elas.\nExiste um conjunto infinito A que não é demasiado grande.\nPodia parecer que chegamos ao limite do que era possível alcançar com a tecnologia dos computadores, contudo, uma pessoa deveria ser cuidadosa com tais afirmações, pois tendem a soar muito tontas em 5 anos. (dito em 1949)"
+    return mensagem
+
+
+def sair():
+    mensagem = "\nObrigado pela leitura!"
+    return mensagem
+
+
+def erro():
+    mensagem = "\nOpção inválida!"
+    return mensagem
+
+
+print("\nBoa noite! Você está aprendendo sobre Allan Turing.\n")
+
+continuar = True
+while continuar == True:
+
+    opcao = int(
+        input(
+"""
+\nDigite o número correspondente ao menu que você deseja acessar:
+1 - Resumo
+2 - Doutorado
+3 - Contribuições
+4 - Principais Artigos
+5 - Citações
+6 - Sair\n
+"""
+        )
+    )
+
+    if opcao == 1:
+        print("1 - Resumo")
+        mensagem = resumo()
+
+    elif opcao == 2:
+        print("2 - Doutorado")
+        mensagem = doutorado()
+
+    elif opcao == 3:
+        print("3 - Contribuições")
+        mensagem = contribuicoes()
+
+    elif opcao == 4:
+        print("4 - Principais Artigos")
+        mensagem = artigos()
+
+    elif opcao == 5:
+        print("5 - Citações")
+        mensagem = citacoes()
+
+    elif opcao == 6:
+        mensagem = sair()
+        continuar = False
+
+    else:
+        mensagem = erro()
+
+    print(mensagem)
